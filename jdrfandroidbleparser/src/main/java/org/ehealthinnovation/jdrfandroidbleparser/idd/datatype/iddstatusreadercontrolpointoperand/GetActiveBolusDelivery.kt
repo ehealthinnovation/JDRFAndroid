8029114b1bf786813f1d3bafd90231d557b59b61
Harry Qiu <cjf12harry@gmail.com>
2018-10-09 22:45:34 -0400

    add comments

--- jdrfandroidbleparser/src/main/java/org/ehealthinnovation/jdrfandroidbleparser/idd/datatype/iddstatusreadercontrolpointoperand/GetActiveBolusDelivery.kt
+++ jdrfandroidbleparser/src/main/java/org/ehealthinnovation/jdrfandroidbleparser/idd/datatype/iddstatusreadercontrolpointoperand/GetActiveBolusDelivery.kt
@@ -3,11 +3,26 @@ package org.ehealthinnovation.jdrfandroidbleparser.idd.datatype.iddstatusreaderc
 import android.bluetooth.BluetoothGattCharacteristic
 import org.ehealthinnovation.jdrfandroidbleparser.encodedvalue.idd.commandcontrolpoint.BolusValueSelectionValue
 
+
+/**
+ * If the Get Active Bolus Delivery Op Code is written to the IDD Status Reader Control Point with
+ *an Operand containing a Bolus ID and a Bolus Value Selection field, the Server shall look up
+ *the corresponding Active Bolus and indicate the IDD Status Reader Control Point with a
+ *Get Active Bolus Delivery Response Op Code and an Active Bolus Delivery record.
+ */
 class GetActiveBolusDelivery : BaseOperandWriter {
 
     override val tag = GetActiveBolusDelivery ::class.java.canonicalName as String
 
+    /**
+     * The Bolus ID field represents a unique identifier as a uint16 data type created by the
+     * Server Application for a programmed bolus.
+     */
     var bolusId: Int? = null
+
+    /**
+     * A type of possible bolus delivery value
+     */
     var bolusValueSelection: BolusValueSelectionValue? = null
 
     constructor(): super()
