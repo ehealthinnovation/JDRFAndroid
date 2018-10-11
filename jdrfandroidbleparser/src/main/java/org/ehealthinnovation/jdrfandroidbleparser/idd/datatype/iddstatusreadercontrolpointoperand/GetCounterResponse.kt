8bca47a68cea3a1a4f19652500314d08cc0c0283
Harry Qiu <cjf12harry@gmail.com>
2018-10-11 10:38:04 -0400

    get counter response

--- jdrfandroidbleparser/src/main/java/org/ehealthinnovation/jdrfandroidbleparser/idd/datatype/iddstatusreadercontrolpointoperand/GetCounterResponse.kt
+++ jdrfandroidbleparser/src/main/java/org/ehealthinnovation/jdrfandroidbleparser/idd/datatype/iddstatusreadercontrolpointoperand/GetCounterResponse.kt
@@ -1,4 +1,25 @@
 package org.ehealthinnovation.jdrfandroidbleparser.idd.datatype.iddstatusreadercontrolpointoperand
 
+import android.bluetooth.BluetoothGattCharacteristic
+import org.ehealthinnovation.jdrfandroidbleparser.encodedvalue.idd.statusreadercontrolpoint.CounterType
+import org.ehealthinnovation.jdrfandroidbleparser.encodedvalue.idd.statusreadercontrolpoint.CounterValueSelection
+
 class GetCounterResponse : BaseOperandParser{
+
+    override val tag = GetCounterResponse ::class.java.canonicalName as String
+
+    constructor(rawData: ByteArray) : super(rawData)
+
+    constructor(rawData: ByteArray, c: BluetoothGattCharacteristic):super(rawData, c)
+
+    var counterType: CounterType? = null
+    var counterValueSelection: CounterValueSelection? = null
+    var counterValue: Int? = null
+
+    override fun parse(): Boolean {
+        counterType = CounterType.fromKey(getNextInt(BluetoothGattCharacteristic.FORMAT_UINT8))
+        counterValueSelection = CounterValueSelection.fromKey(getNextInt(BluetoothGattCharacteristic.FORMAT_UINT8))
+        counterValue = getNextInt(BluetoothGattCharacteristic.FORMAT_UINT32)
+        return true
+    }
 }
